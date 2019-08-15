c69ae4d73901c84ef9c4b508fbc4a4a342d1cb01
newstreetpunk <newstreetpunk@yandex.ru>
2019-08-15 11:03:41 +0500

    df79

--- js/common.js
+++ js/common.js
@@ -2,6 +2,10 @@ jQuery(function($) {
 
 	$('.mobile-btn').on('click', function() {
 		$(this).toggleClass('active');
+		$('.menu').toggleClass('active');
+	});
+	$('.menu ul li').on('click', function() {
+		$('.mobile-btn, .menu').removeClass('active');
 	});
 
 
