296cc7f3df33a954676910ed226b4f811d9c1b0e
newstreetpunk <newstreetpunk@yandex.ru>
2019-08-15 22:27:49 +0500

    df79

--- js/common.js
+++ js/common.js
@@ -8,5 +8,23 @@ jQuery(function($) {
 		$('.mobile-btn, .menu').removeClass('active');
 	});
 
+	$('.checkbox input').on('change', function(){
+		if($(this).is(':checked')) $('.intro-form form input[type="submit"]').attr('disabled', false);
+		else $('.intro-form form input[type="submit"]').attr('disabled', true);
+	});
+
+	$('.read-btn').on('click', function() {
+		if($(this).is(':contains("Подробнее")')){
+			$('.about__text--hidden').slideDown(300);
+			$('.about').css('overflow-y', 'scroll');
+			$(this).text('Скрыть');
+		}else{
+			$(this).text('Подробнее');
+			$('.about__text--hidden').slideUp(100);
+			$('.about').removeAttr('style');
+		}
+		
+	});
+
 
 });
